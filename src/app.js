01bb1de7679c1450b34e0138210e23eae2785ae4
samuelpauloantonio <samuelpauloantonio1@gmail.com>
2021-03-13 17:25:46 +0100

    desafio completo conceitos_Nodejs

--- src/app.js
+++ src/app.js
@@ -1,33 +1,120 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const repositories = [];
+const repositories = []; 
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  
+  return response.json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  
+  const { title , url , techs } = request.body
+
+  const createRepositorie = {
+    id : uuid(),
+    title,
+    url,
+    techs,
+    likes : 0
+
+  }
+
+  repositories.push(createRepositorie)
+
+
+  return response.json(createRepositorie)
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  
+  const  { id } = request.params                                 
+  
+
+  let  myIndex = 0;
+  const findRespositorie = repositories.find((item, index) => {
+    if(item.id == id){
+      myIndex = index
+
+      return item
+    }
+
+  })
+
+
+  if(!findRespositorie) return response.status(400).json({
+    error : "invalid id  repositorie or not found respositorie"
+  })
+
+
+  const updated = repositories[myIndex] = {
+    ...findRespositorie ,
+    ...request.body,
+    likes:repositories[myIndex].likes,
+  }
+
+
+
+  return response.json(updated)
+
+
+
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
-});
+  
+ 
+  const  { id } = request.params
+  
+
+  const findIndexRepositorie = repositories.findIndex(item => item.id == id)
+
+  if(!isUuid(id) &&  findIndexRepositorie < 0) return response.status(400).json({
+    error : "invalid id  repositorie or not found respositorie to  delete"
+  })
+
+
+    repositories.splice(findIndexRepositorie, 1)
+
+    return response.status(204).send()
+
+
+
+
+}); 
+
+
+
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  
+  const { id } = request.params 
+
+  const repositorie = repositories.find(item => item.id == id)
+
+
+  if(!isUuid(id) && !repositorie) return response.status(400).json({
+    error : "invalid id  repositorie or not found respositorie"
+  })
+  
+    repositorie.likes++
+
+
+    return response.json(repositorie)  
 });
+
+
+
 
 module.exports = app;
